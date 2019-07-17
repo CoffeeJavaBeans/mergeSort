528dd7c7d063bec89b7ca6387e8fc6a735a7c6d7
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-07-17 15:24:44 -0500

    merge function works

--- mergesort.js
+++ mergesort.js
@@ -12,5 +12,21 @@ function split(wholeArray) {
   return [firstHalf, secondHalf];
 }
 
-let arrr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(split(arrr));
+function merge(arr1, arr2) {
+  let sorted = [];
+  while (arr1.length > 0 || arr2.length > 0) {
+    if (arr1[0] <= arr2[0]) {
+      sorted.push(arr1.shift());
+    } else {
+      sorted.push(arr2.shift());
+    }
+  }
+  return sorted;
+}
+
+//let arrr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//console.log(split(arrr));
+
+let arr1 = [1, 4, 6];
+let arr2 = [2, 3, 7, 8];
+console.log(merge(arr1, arr2));
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -3,3 +3,8 @@
 //       // your code here
 //     });
 //   });
+// describe('Merge function', function(){
+//     it('is able to merge two sorted arrays into one sorted array', function(){
+//       // test the merging algorithm
+//     });
+//   });
